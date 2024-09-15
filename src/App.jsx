cc7e48282791069836c6d223c4e4e72b4c4bf46e
DESKTOP-VN3VINC\Дмитро <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-15 17:04:04 +0300

    Feat: added Vacancy and Index pages; added navigation using react-router;

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes,Route } from "react-router";
+
+import Vacancy from "./Pages/Vacancy/Vacancy";
+import Index from "./Pages/Index/Index";
+import { Link } from "react-router-dom";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+            <Route path='/vite-react-app/' element={<Index/>}/>
+            <Route path='/vite-react-app/vacancy' element={<Vacancy/>}/>
+     </Routes>
+      
+      <Link to="/vite-react-app/">Index</Link>
+      <Link to="/vite-react-app/vacancy">Vacancy</Link>
+      
+      
     </>
   )
 }
---
+++ src/Pages/Vacancy/Vacancy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Vacancy() {
+    return (
+        <div>
+                  <h1>Vacancy Page</h1>
+
+        </div>
+    );
+}
+
+export default Vacancy;
