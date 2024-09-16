af08df24d1235bb083b659ea0ba3507183f22c79
Khrystyna-Front-end <121770905+Khrystyna-Front-end@users.noreply.github.com>
2024-09-16 19:41:15 +0300

    Removed color in App.scss, Styling Nav.scss

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,33 +1,24 @@
- import React from "react";
- import flag from "../../images/flag-nav.png";
- import frame from '../../images/frame-nav.png';
- import './nav.scss';
+import React from "react";
+import flag from "../../assets/indexFlagNav.png";
+import frame from "../../assets/indexFrameNav.png";
+import "../Nav/Nav.scss";
 
-export default function Nav(){
-return (
-  <nav>
-    <div classNameName="nav-block-left">
-      <img
-        classNameName="img-logo"
-        src={frame}
-        alt="img-logo"
-      />
-      <h2 classNameName="name-logo">Buxonline</h2>
-    </div>
-    <div className="nav-block-right">
-      <h3>ВАКАНЦІЇ</h3>
-      <h3>ДЛЯ БІЗНЕСУ</h3>
-      <a className="nav-entrance" href="#">
-        Вхід / Реєстрація 
-      </a>
-      <img className="flag-nav" src={flag} alt="flag-nav" />
-      <h4>UK</h4>
-    </div>
-  </nav>
-);
+export default function Nav() {
+  return (
+    <nav>
+      <div classNameName="nav-block-left">
+        <img classNameName="img-logo" src={frame} alt="img-logo" />
+        <h2 classNameName="name-logo">Buxonline</h2>
+      </div>
+      <div className="nav-block-right">
+        <h3>ВАКАНЦІЇ</h3>
+        <h3>ДЛЯ БІЗНЕСУ</h3>
+        <a className="nav-entrance" href="#">
+          Вхід / Реєстрація
+        </a>
+        <img className="flag-nav" src={flag} alt="flag-nav" />
+        <h4>UK</h4>
+      </div>
+    </nav>
+  );
 }
- 
-
-       
-        
-   
