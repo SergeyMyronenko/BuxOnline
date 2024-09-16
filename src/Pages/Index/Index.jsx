34d1050cb1fdcf07d009f43f92b947b8cacc1e6e
Khrystyna-Front-end <121770905+Khrystyna-Front-end@users.noreply.github.com>
2024-09-17 02:11:18 +0300

    Edited Footer.jsx (changed className), Edited Footer .scss

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Nav from "../../Components/Nav/Nav";
 import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
 import './Index.scss';
 
 function Index() {
     return (
-            <Header/>
+      <>
+        {" "}
+        <Header />
+        <Nav />
+        <Footer/>
+      </>
     );
 };
 
