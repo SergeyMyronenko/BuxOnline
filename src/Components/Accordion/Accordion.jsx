6b0c699462b80fe9f2e53a1444e6c43712d3baf0
DESKTOP-VN3VINC\Дмитро <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-16 10:27:38 +0300

    Feat: Accordion component that has dynamic height

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Accordion.scss';
+import { useState } from 'react';
+import { FaAngleDown } from "react-icons/fa6";
 
 const Accordion = ({ accordionTitle, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className='accordion-wrapper'>
             <div className="accordion-title">
                 <h3 >{accordionTitle}</h3>
-                <div>{'\u{1F893}'}</div>
+                <div className={`accordion-button ${isOpen?'open':''}`} onClick={()=>setIsOpen(!isOpen)} >
+                <FaAngleDown></FaAngleDown>
+                <FaAngleDown></FaAngleDown>
+                </div>
             </div>
 
-            <ul className='accordion-list'>
-                {React.Children.map(children, (child, index) => (                    
-                    <li className='accordion-item' key={index}>
-                        <input type="checkbox" name="" id="" />
-                        {child}                   
-                    </li>
-                ))}
+            <ul className={`accordion-list ${isOpen?'open':''}`}>
+                <div className='accordion-items-wrapper'>
+                    {React.Children.map(children, (child, index) => (
+                        <li className='accordion-item' key={index}>
+                            <input type="checkbox" name="" id="" />
+                            {child}
+                        </li>
+                    ))}
+                </div>
             </ul>
         </div>
     );
