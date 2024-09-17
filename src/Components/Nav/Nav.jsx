2aff0221069c63ca634840a92a23993a91d80d6d
Khrystyna-Front-end <121770905+Khrystyna-Front-end@users.noreply.github.com>
2024-09-17 13:39:36 +0300

    Update Nav.jsx and Footer.jsx

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import flag from "../../assets/indexFlagNav.png";
 import frame from "../../assets/indexFrameNav.png";
+import { TiArrowSortedDown } from "react-icons/ti";
+
+
 import "../Nav/Nav.scss";
 
 export default function Nav() {
+   const [language, setLanguage] = useState("UK");
+
+  const changeLanguage = () => {
+    setLanguage((prevLanguage) => (prevLanguage === "UK" ? "US" : "UK"));
+  };
+
   return (
     <nav>
-      <div classNameName="nav-block-left">
-        <img classNameName="img-logo" src={frame} alt="img-logo" />
-        <h2 classNameName="name-logo">Buxonline</h2>
+      <div className="nav-block-left">
+        <img className="img-logo" src={frame} alt="img-logo" />
+        <h2 className="name-logo">Buxonline</h2>
       </div>
       <div className="nav-block-right">
         <h3>ВАКАНЦІЇ</h3>
@@ -16,8 +25,12 @@ export default function Nav() {
         <a className="nav-entrance" href="#">
           Вхід / Реєстрація
         </a>
-        <img className="flag-nav" src={flag} alt="flag-nav" />
-        <h4>UK</h4>
+        <button className="button-language-change" onClick={changeLanguage}>
+          <img className="flag-nav" src={flag} alt="flag-nav" />
+          <h4>{language}</h4>
+          <TiArrowSortedDown />
+          {/* <img className="language-arrow" src={arrowDown} alt = "language-arrow"/> */}
+        </button>
       </div>
     </nav>
   );
