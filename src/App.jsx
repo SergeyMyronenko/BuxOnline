4acab47bc2c88995650c331fae52033fed858958
Sergiy Myronenko <Sergiymyronenko.web@gmail.com>
2024-09-30 15:58:52 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,12 @@ import EmptyOutlet from "./Pages/EmptyOutlet/EmptyOutlet.jsx";
 import "@mantine/core/styles.css";
 import "./App.scss";
 
+<<<<<<< HEAD
+=======
 import "@mantine/core/styles.css";
 import VacancyCreated from "./Pages/EmployerPage/VacancyCreated.tsx";
 import AdminPage from "./Pages/AdminPage/AdminPage.jsx";
+>>>>>>> admin-page
 
 /**
  * The main application component that sets up the routing for the application.
@@ -40,63 +43,48 @@ function App() {
     no time for that rn
     */}
 
+
       <Routes>
-        <Route path="/BuxOnline/" element={<Index />} />
+        <Route path='/BuxOnline/' element={<Index />} />
 
-        <Route path="/BuxOnline/vacancies" element={<AllVacancies />} />
-        <Route
-          path="/BuxOnline/vacancies/:id"
-          element={<VacancyDescription />}
-        />
+        <Route path='/BuxOnline/vacancies' element={<AllVacancies />} />
+        <Route path='/BuxOnline/vacancies/:id' element={<VacancyDescription />} />
 
-        <Route path="/BuxOnline/login" element={<LogIn />} />
-        <Route
-          path="/BuxOnline/login/forgot-password"
-          element={<ForgotPassword />}
-        />
-        <Route
-          path="/BuxOnline/login/forgot-password/:email"
-          element={<ForgotPassword />}
-        />
+        <Route path='/BuxOnline/login' element={<LogIn />} />
+        <Route path='/BuxOnline/login/forgot-password' element={<ForgotPassword />} />
+        <Route path='/BuxOnline/login/forgot-password/:email' element={<ForgotPassword />} />
 
-        <Route path="/BuxOnline/register" element={<Register />} />
+        <Route path='/BuxOnline/register' element={<Register />} />
 
-        <Route
-          path="/BuxOnline/company/cabinet/:id"
-          element={<CompanyCabinet />}
-        ></Route>
-        <Route
-          path="/BuxOnline/company/jobs/"
-          element={<CompanyVacancies />}
-        ></Route>
-        <Route
-          path="/BuxOnline/company/jobs/choose"
-          element={<ChooseMethodToCreate />}
-        ></Route>
-        <Route
-          path="/BuxOnline/company/jobs/create"
-          element={<CreateVacancy />}
-        ></Route>
-        <Route
-          path="/BuxOnline/company/jobs/create/:id"
-          element={<ConfirmCreatedVacancy />}
-        ></Route>
-        <Route
-          path="/BuxOnline/company/jobs/create/:id/finished"
-          element={<VacancyCreated />}
-        ></Route>
+<<<<<<< HEAD
+        {/* These are all the routes and pages related to company cabinet */}
+        <Route path="/BuxOnline/company/cabinet/:id" element={<EmptyOutlet />}>
+        {/* the line below is responsible for the main page */}
+          <Route index element={<CompanyCabinet />} />
+          {/* all the other lines below are different paths and pages inside the cabinet */}
+          <Route path="jobs" element={<CompanyVacancies />} />
+          <Route path="messages" element={<CompanyMessage />} />
+          <Route path="candidates" element={<h1>Company Candidates</h1>} />
+          <Route path="favorites" element={<h1>Company Favorites</h1>} />
+        </Route>
+=======
+        <Route path="/BuxOnline/company/cabinet/:id" element={<CompanyCabinet/>}></Route>      
+        <Route path="/BuxOnline/company/jobs/" element={<CompanyVacancies/>}></Route>    
+        <Route path="/BuxOnline/company/jobs/choose" element={<ChooseMethodToCreate/>}></Route>   
+        <Route path="/BuxOnline/company/jobs/create" element={<CreateVacancy/>}></Route>   
+        <Route path="/BuxOnline/company/jobs/create/:id" element={<ConfirmCreatedVacancy/>}></Route>    
+        <Route path="/BuxOnline/company/jobs/create/:id/finished" element={<VacancyCreated/>}></Route>    
 
-        <Route
-          path="/BuxOnline/seeker/profile/resume"
-          element={<SeekerResumePage />}
-        ></Route>
-        <Route path="/BuxOnline/moderator" element={<AdminPage />}></Route>
+        <Route path="/BuxOnline/seeker/profile/resume" element={<SeekerResumePage/>}></Route>     
+        <Route path="/BuxOnline/moderator" element={<AdminPage />}></Route> 
+>>>>>>> admin-page
+
 
         {/* These are all the routes and pages related to seeker cabinet */}
         <Route path="/BuxOnline/seeker/cabinet/:id" element={<EmptyOutlet />}>
-          {/* the line below is responsible for the main page */}
+        {/* the line below is responsible for the main page */}
           <Route index element={<h1>Seeker cabinet</h1>} />
-
+          
           {/* all the other lines below are different paths and pages inside the cabinet */}
           <Route path="resumes" element={<SeekerResumePage />} />
           <Route path="offers" element={<h1>Seeker offers</h1>} />
@@ -104,12 +92,10 @@ function App() {
           <Route path="favorites" element={<h1>Seeker Favorites</h1>} />
         </Route>
 
+
         {/* These are all the routes and pages related to moderator cabinet */}
-        <Route
-          path="/BuxOnline/moderator/cabinet/:id"
-          element={<EmptyOutlet />}
-        >
-          {/* the line below is responsible for the main page */}
+        <Route path="/BuxOnline/moderator/cabinet/:id" element={<EmptyOutlet />}>
+        {/* the line below is responsible for the main page */}
           <Route index element={<h1>Moderator cabinet</h1>} />
           {/* all the other lines are different paths and pages inside the cabinet */}
           <Route path="resumes" element={<SeekerResumePage />} />
@@ -118,7 +104,8 @@ function App() {
           <Route path="favorites" element={<h1>Seeker Favorites</h1>} />
         </Route>
 
-        <Route path="*" element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
+
       </Routes>
     </>
   );
