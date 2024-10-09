81ec98e14b2f65e0216b681dfb5d757cb0636931
Vadym Havryliuk <vadimg692@gmail.com>
2024-10-09 22:30:19 +0300

    change DetailsCandidate, PersonalData, ContactData

--- src/Pages/EmployerPage/CompanyFavorites/ContactData/ContactData.jsx
+++ src/Pages/EmployerPage/CompanyFavorites/ContactData/ContactData.jsx
@@ -4,7 +4,23 @@ import './ContactData.scss';
 const ContactData = () => {
     return (
         <div className="contact-data">
+            <div className="contact-data-wrapper">
+                <h3 className="contact-data-title">Контакти</h3>
+                <div className="contacts-email-phone">
+                    <div className="email">
 
+                    </div>
+                    <div className="number">
+                        
+                    </div>
+                </div>
+                <div className="contacts-socials">
+
+                </div>
+                <div className="contact-data-buttons">
+
+                </div>
+            </div>
         </div>
     );
 }
