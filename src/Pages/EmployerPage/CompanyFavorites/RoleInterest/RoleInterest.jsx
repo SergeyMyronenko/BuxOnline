5dd44a1348054749e61ac4e3a912e0edb08dabe8
Vadym Havryliuk <vadimg692@gmail.com>
2024-10-10 12:52:16 +0300

    chnage RoleInterest and Lanuages

--- src/Pages/EmployerPage/CompanyFavorites/RoleInterest/RoleInterest.jsx
+++ src/Pages/EmployerPage/CompanyFavorites/RoleInterest/RoleInterest.jsx
@@ -4,7 +4,18 @@ import './RoleInterest.scss';
 const RoleInterest = () => {
     return (
         <div className="role-interest">
-
+            <div className="role-interst-info">
+                <span className="role-position">Full-stack engineer</span>
+                <span className="role-experience">3 роки</span>
+            </div>
+            <div className="role-interst-info">
+                <span className="role-position">Front-end engineer</span>
+                <span className="role-experience">3 роки</span>
+            </div>
+            <div className="role-interst-info">
+                <span className="role-position">Beck-end engineer</span>
+                <span className="role-experience">3 роки</span>
+            </div>
         </div>
     );
 };
